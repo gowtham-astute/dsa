8b64ae1804efed18d8960dcc761043976684644b
Gowtham <gowthamastute@gmail.com>
2024-10-18 23:48:45 +0530

    bubble sort logic added to minimize loop execution in best case

--- src/main/java/org/dsa/sorting/BubbleSort.java
+++ src/main/java/org/dsa/sorting/BubbleSort.java
@@ -5,19 +5,24 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void sort(int arr[]){
         for(int i=0;i<arr.length;i++){
+            boolean swapped = false;
             for(int j=0;j<arr.length-i-1;j++){
                 if(arr[j]>arr[j+1]){
+                    swapped =true;
                     int temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
                 }
+            }
+            if(!swapped){
+                break;
             }
         }
     }
 
     public static void main(String[] args) {
 
-        int arr[] = {100,80,30,12,45,22,90,67,87,99,102};
+        int arr[] = {100,80,30,12,45,22,90,67,12,87,99,102};
         BubbleSort.sort(arr);
         System.out.println(Arrays.toString(arr));
     }
