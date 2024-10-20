b76374846812bb33b725c622b69abea7790d2278
Gowtham <gowthamastute@gmail.com>
2024-10-20 22:16:34 +0530

    updated code

--- src/main/java/org/dsa/basic/FindEven.java
+++ src/main/java/org/dsa/basic/FindEven.java
@@ -2,10 +2,10 @@ package org.dsa.basic;
 
 public class FindEven {
 
-    public static void findEven(int arr[]){
+    public static void validate(int arr[]){
         int count=0;
         for(int i=0;i<arr.length;i++){
-            if(arr[0]%2==0){
+            if(arr[i]%2==0){
                 count++;
             }
         }
@@ -15,5 +15,12 @@ public class FindEven {
             System.out.println("Not A Even Number");
         }
 
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {10,13,11,10,9,17,21};
+        int arr2[] = {2,4,6,8,10};
+        FindEven.validate(arr);
+        FindEven.validate(arr2);
     }
 }
