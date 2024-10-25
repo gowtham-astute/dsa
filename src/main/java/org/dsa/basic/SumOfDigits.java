074a10c4a28ec1d345fd81e214c2382715b24601
Gowtham <gowthamastute@gmail.com>
2024-10-25 17:01:46 +0530

    variation program in calculating sum of digits

--- src/main/java/org/dsa/basic/SumOfDigits.java
+++ src/main/java/org/dsa/basic/SumOfDigits.java
@@ -11,8 +11,25 @@ public class SumOfDigits {
         return sum;
     }
 
+    //Given a value of N, print only those numbers from 1 to N that have sum of digits = K
+    public static void sum(int n, int k){
+        for(int i=1;i<=n;i++){
+            int sum=0;
+            for (int j=i;j>0;j/=10){
+                int lastDigit=j%10;
+                sum+=lastDigit;
+            }
+            if(sum==k){
+                System.out.println("Result is "+i);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(SumOfDigits.sum(163425));
         System.out.println(SumOfDigits.sum(12345));
+
+        SumOfDigits.sum(12,3);
+        SumOfDigits.sum(123, 12);
     }
 }
