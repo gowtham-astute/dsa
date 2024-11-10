eb8c47ded4624f79e0ad9f820f0bb40b57a9e89f
Gowtham <gowthamastute@gmail.com>
2024-11-10 23:37:31 +0530

    dsa basic program

---
+++ src/main/java/org/dsa/basic/array/MaxElementInAMatrix.java
@@ -0,0 +1,33 @@
+package org.dsa.basic.array;
+
+/**
+ * Program to find the maximum element in a Matrix
+ * Input: mat[4][4] = {{1, 2, 3, 4},
+ *                     {25, 6, 7, 8},
+ *                     {9, 10, 11, 12},
+ *                     {13, 14, 15, 16}};
+ *
+ */
+public class MaxElementInAMatrix {
+
+    public static void findMax(int[][] arr){
+        int max=arr[0][0];
+        for (int i=0; i< arr.length; i++){
+            for (int j=0; j< arr[i].length; j++){
+                if(arr[i][j] > max){
+                    max= arr[i][j];
+                }
+            }
+        }
+        System.out.println("Max element is "+max);
+    }
+
+    public static void main(String[] args) {
+        int mat[][] = { { 1, 2, 3, 4 },
+                { 25, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 55, 16 } };
+
+        MaxElementInAMatrix.findMax(mat);
+    }
+}
