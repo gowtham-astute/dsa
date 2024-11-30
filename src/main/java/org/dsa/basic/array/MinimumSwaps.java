00552a6d520a345a719611d90237dde0eba069fd
Gowtham <gowthamastute@gmail.com>
2024-11-30 23:55:28 +0530

    dsa

---
+++ src/main/java/org/dsa/basic/array/MinimumSwaps.java
@@ -0,0 +1,41 @@
+package org.dsa.basic.array;
+//Minimum Swaps to Sort
+
+/**
+ * Given an array arr[] of distinct elements.
+ * Find the minimum number of swaps required to sort the array in strictly increasing order.
+ * Input: arr[] = [10, 19, 6, 3, 5]
+ * Output: 2
+ * Explanation: swap 10 with 3 and swap 19 with 5.
+ */
+public class MinimumSwaps {
+
+    public static int minSwaps(int arr[]) {
+        int count=0;
+        for (int i=0; i<arr.length; i++){
+            int minIndex=i; int maxIndex=i;
+            for (int j=i; j<arr.length; j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+                if(arr[j]>arr[maxIndex]){
+                    maxIndex=j;
+                }
+            }
+            if(minIndex>maxIndex){
+                count++;
+                int temp=arr[minIndex];
+                arr[minIndex]=arr[maxIndex];
+                arr[maxIndex]=temp;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int  arr[] = {10, 19, 6, 3, 5};
+        int arr1[] = {2, 8, 5, 4};
+        System.out.println(MinimumSwaps.minSwaps(arr1));
+    }
+
+}
