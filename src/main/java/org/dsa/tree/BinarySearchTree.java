b4ead2729951de22fd49fbe65d79c4571e030559
Gowtham <gowthamastute@gmail.com>
2024-12-08 23:52:32 +0530

    dsa program

--- src/main/java/org/dsa/tree/BinarySearchTree.java
+++ src/main/java/org/dsa/tree/BinarySearchTree.java
@@ -3,51 +3,53 @@ package org.dsa.tree;
 public class BinarySearchTree {
 
     Node root;
-    class Node{
+
+    class Node {
         int data;
         Node left;
         Node right;
+
         public Node(int data) {
             this.data = data;
             left = null;
-            right =null;
+            right = null;
         }
     }
 
-    BinarySearchTree(){
+    BinarySearchTree() {
         root = null;
     }
-    BinarySearchTree(int data){
+
+    BinarySearchTree(int data) {
         root = new Node(data);
     }
 
     //insert
-    public void insert(int data){
+    public void insert(int data) {
         root = insert(data, root);
     }
 
-    private Node insert(int data, Node root){
+    private Node insert(int data, Node root) {
 
-        if(root==null){
+        if (root == null) {
             return new Node(data);
         }
-        if(data < root.data){
-          root.left =  insert(data, root.left);
-        }
-        else if (data > root.data){
-          root.right =  insert(data, root.right);
+        if (data < root.data) {
+            root.left = insert(data, root.left);
+        } else if (data > root.data) {
+            root.right = insert(data, root.right);
         }
 
         return root;
     }
 
     //traverse
-    public void inOrderTraversal(){
+    public void inOrderTraversal() {
         inOrderTraversal(root);
     }
 
-    private void inOrderTraversal(Node node){
-        if(node==null)
+    private void inOrderTraversal(Node node) {
+        if (node == null)
             return;
         inOrderTraversal(node.left);
         System.out.println(node.data);
@@ -55,34 +57,33 @@ public class BinarySearchTree {
     }
 
     //delete
-    public void delete(int data){
-      root =  delete(data, this.root);
+    public void delete(int data) {
+        root = delete(data, this.root);
     }
 
-    private Node delete(int data, Node root){
+    private Node delete(int data, Node root) {
 
-        if(root == null){
+        if (root == null) {
             return null;
         }
 
-        if(data < root.data){
-         root.left =   delete(data, root.left);
-        }
-        else if(data > root.data){
-         root.right =  delete(data, root.right);
+        if (data < root.data) {
+            root.left = delete(data, root.left);
+        } else if (data > root.data) {
+            root.right = delete(data, root.right);
         }
 
         //having single child or no child
-        if(root.right==null){
+        if (root.right == null) {
             return root.left;
-        } else if (root.left==null) {
+        } else if (root.left == null) {
             return root.right;
         }
 
         //having 2 child
-       int minElement = findMinElement(root.right);
-        root.data=minElement;
-        root.right = delete(minElement,root.right);
+        int minElement = findMinElement(root.right);
+        root.data = minElement;
+        root.right = delete(minElement, root.right);
 
         return root;
     }
