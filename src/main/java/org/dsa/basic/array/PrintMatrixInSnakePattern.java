6f377ee58f6bb53d52a4b3081d350a3e126322a4
Gowtham <gowthamastute@gmail.com>
2024-12-14 23:25:40 +0530

    dsa

--- src/main/java/org/dsa/basic/array/PrintMatrixInSnakePattern.java
+++ src/main/java/org/dsa/basic/array/PrintMatrixInSnakePattern.java
@@ -4,16 +4,15 @@ import java.util.ArrayList;
 
 public class PrintMatrixInSnakePattern {
 
-    public static ArrayList<Integer> snakePattern(int matrix[][])
-    {
+    public static ArrayList<Integer> snakePattern(int matrix[][]) {
         ArrayList<Integer> al = new ArrayList<>();
-        for(int i=0;i< matrix.length;i++){
-            if(i%2==0){
-               for (int j=0;j<matrix[i].length;j++){
-                   al.add(matrix[i][j]);
-               }
-            }else{
-                for (int j=matrix[i].length-1;j>=0;j--){
+        for (int i = 0; i < matrix.length; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    al.add(matrix[i][j]);
+                }
+            } else {
+                for (int j = matrix[i].length - 1; j >= 0; j--) {
                     al.add(matrix[i][j]);
                 }
             }
@@ -22,7 +21,7 @@ public class PrintMatrixInSnakePattern {
     }
 
     public static void main(String[] args) {
-       int matrix[][] = {{45, 48, 54}, {21, 89, 87}, {70, 78, 15}};
+        int matrix[][] = {{45, 48, 54}, {21, 89, 87}, {70, 78, 15}};
         System.out.println(PrintMatrixInSnakePattern.snakePattern(matrix));
     }
 }
