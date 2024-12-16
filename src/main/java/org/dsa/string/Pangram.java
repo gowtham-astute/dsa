3f1f1fd21a0b63fc8fabef6f2ec3f5f66b35da7b
Gowtham <gowthamastute@gmail.com>
2024-12-16 23:11:53 +0530

    dsa

--- src/main/java/org/dsa/string/Pangram.java
+++ src/main/java/org/dsa/string/Pangram.java
@@ -3,14 +3,14 @@ package org.dsa.string;
 /**
  * Given a string s, the task is to check if it is Pangram or not.
  * A pangram is a sentence containing all letters of the English Alphabet.
- *
+ * <p>
  * Examples:
- *
+ * <p>
  * Input: s = “The quick brown fox jumps over the lazy dog”
  * Output: true
  * Explanation: The input string contains all characters from ‘a’ to ‘z’.
- *
- *
+ * <p>
+ * <p>
  * Input: s = “The quick brown fox jumps over the dog”
  * Output: false
  * Explanation: The input string does not contain all characters from ‘a’ to ‘z’, as ‘l’, ‘z’, ‘y’
@@ -18,20 +18,21 @@ package org.dsa.string;
  */
 public class Pangram {
 
-    public static boolean checkPangram(String str){
-        boolean result=false;
-        for (char c='a'; c <='z'; c++){
-            for (int i=0; i<str.length(); i++){
-                if(c==str.toLowerCase().charAt(i)){
+    public static boolean checkPangram(String str) {
+        boolean result = false;
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < str.length(); i++) {
+                if (c == str.toLowerCase().charAt(i)) {
                     result = true;
                     break;
                 }
             }
-            if(!result)
+            if (!result)
                 return false;
         }
         return result;
     }
+
     public static void main(String[] args) {
         String str = "The quick brown fox jumps over the dog";
         System.out.println(Pangram.checkPangram(str));
