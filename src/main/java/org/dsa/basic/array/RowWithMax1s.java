6ca2dfa344381406f7bca1a35cd62255e3003450
Gowtham <gowthamastute@gmail.com>
2024-12-18 23:51:50 +0530

    dsa

--- src/main/java/org/dsa/basic/array/RowWithMax1s.java
+++ src/main/java/org/dsa/basic/array/RowWithMax1s.java
@@ -5,39 +5,39 @@ package org.dsa.basic.array;
  * You need to find and return the index of the first row that has the most number of 1s.
  * If no such row exists, return -1.
  * Note: 0-based indexing is followed.
- *
+ * <p>
  * Examples:
- *
+ * <p>
  * Input: arr[][] = [[0, 1, 1, 1],
- *                [0, 0, 1, 1],
- *                [1, 1, 1, 1],
- *                [0, 0, 0, 0]]
+ * [0, 0, 1, 1],
+ * [1, 1, 1, 1],
+ * [0, 0, 0, 0]]
  * Output: 2
  * Explanation: Row 2 contains 4 1's.
  */
 public class RowWithMax1s {
 
     public static int rowWithMax1s(int arr[][]) {
-        int max1srow=-1;
-       for (int i=0; i< arr.length; i++){
-           int count=0;
-           for (int j=0; j<arr[i].length; j++){
-               if(arr[i][j] >= 1 ){
-                   count++;
-               }
-           }
-           if(count>0){
-               max1srow=i;
-           }
-       }
-       return max1srow;
+        int max1srow = -1;
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] >= 1) {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                max1srow = i;
+            }
+        }
+        return max1srow;
     }
 
     public static void main(String[] args) {
-       int arr[][] = {{0, 1, 1, 1},
-               {0, 0, 1, 1},
-               {1, 1, 1, 1},
-               {0, 0, 0, 0}};
+        int arr[][] = {{0, 1, 1, 1},
+                {0, 0, 1, 1},
+                {1, 1, 1, 1},
+                {0, 0, 0, 0}};
         System.out.println(RowWithMax1s.rowWithMax1s(arr));
     }
 }
